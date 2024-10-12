3588e183f420227db0ff699c70551cf0c17dc135
dyeisonFK <dyeisonfc@gmail.com>
2024-10-12 16:05:49 -0300

    feat: add page login and function viewPassword

--- src/page/login/Login.tsx
+++ src/page/login/Login.tsx
@@ -1,5 +1,142 @@
-export default function Login() {
+'use client';
+
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Mail, Lock, User, Eye, EyeOff } from 'lucide-react';
+
+export default function AuthForm() {
+  const [activeTab, setActiveTab] = useState('login');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Aqui você pode lidar com o envio do formulário
+    console.log('Form submitted');
+  };
+
   return (
-    <div>Login</div>
-  )
+    <section className='bg-primary h-full min-h-screen flex flex-col justify-center items-center'>
+      <img src="/LogoBarbeiro.svg" alt="Logo" className='w-60'/>
+      <Card className="w-[350px] bg-white">
+        <CardHeader>
+          <CardTitle>Autenticação</CardTitle>
+          <CardDescription>Faça login ou crie uma nova conta.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Tabs value={activeTab} onValueChange={setActiveTab}>
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="login" className={`${activeTab === 'login' ? 'bg-primary': 'bg-white'}`}>Login</TabsTrigger>
+              <TabsTrigger value="register" className={`${activeTab === 'register' ? 'bg-primary': 'bg-white'}`}>Cadastro</TabsTrigger>
+            </TabsList>
+            <TabsContent value="login">
+              <form onSubmit={handleSubmit}>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="login-email">Email</Label>
+                    <div className="relative">
+                      <Mail className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                      <Input
+                        id="login-email"
+                        type="email"
+                        placeholder="seu@email.com"
+                        className="pl-8"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="login-password">Senha</Label>
+                    <div className="relative">
+                      <Lock className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                      <Input
+                        id="login-password"
+                        type={showPassword ? 'text' : 'password'}
+                        className="pl-8 pr-8"
+                        required
+                      />
+                      <div
+                        onClick={handleTogglePasswordVisibility}
+                        className="absolute right-2 top-2.5 h-4 w-4 text-muted-foreground cursor-pointer"
+                      >
+                        {showPassword ? <EyeOff /> : <Eye />}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <Button className="w-full mt-4" type="submit">
+                  Entrar
+                </Button>
+              </form>
+            </TabsContent>
+            <TabsContent value="register">
+              <form onSubmit={handleSubmit}>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="register-name">Nome</Label>
+                    <div className="relative">
+                      <User className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                      <Input
+                        id="register-name"
+                        placeholder="Seu nome"
+                        className="pl-8"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="register-email">Email</Label>
+                    <div className="relative">
+                      <Mail className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                      <Input
+                        id="register-email"
+                        type="email"
+                        placeholder="seu@email.com"
+                        className="pl-8"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="register-password">Senha</Label>
+                    <div className="relative ">
+                      <Lock className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                      <Input
+                        id="register-password"
+                        type={showPassword ? 'text' : 'password'}
+                        className="pl-8 pr-8"
+                        required
+                      />
+                      <div
+                        onClick={handleTogglePasswordVisibility}
+                        className="absolute right-2 top-2.5 h-4 w-4 text-muted-foreground cursor-pointer"
+                      >
+                        {showPassword ? <EyeOff /> : <Eye />}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <Button className="w-full mt-4" type="submit">
+                  Cadastrar
+                </Button>
+              </form>
+            </TabsContent>
+          </Tabs>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
