a0a553e0a9ef81be1fe0552296bcba8cfaebb789
dyeisonFK <dyeisonfc@gmail.com>
2024-10-14 14:51:52 -0300

    fix: correct type in ContextType

--- src/@types/contextType.ts
+++ src/@types/contextType.ts
@@ -1,7 +1,9 @@
 export type ContextType = {
   activeTab: string;
-  setActiveTab: (tab: string) => void;
+  setActiveTab: React.Dispatch<React.SetStateAction<string>>;
+  //React.Dispatch:  É um tipo que representa uma função que aceita um valor (ou uma função) como argumento e, em seguida, executa essa ação.
+  // <React.SetStateAction<string>>: É um tipo que pode ser usado para atualizar o estado. Pode ser um valor direto ou uma função que recebe o estado atual e retorna o novo estado.
   showPassword: boolean;
-  setShowPassword: (tab: boolean) => void;
+  setShowPassword: React.Dispatch<React.SetStateAction<boolean>>;
   togglePasswordVisibility: () => void;
 }
