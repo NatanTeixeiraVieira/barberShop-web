c5396548f90437f5834a328a019e8b2d6e4e159d
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-11-15 13:42:54 -0300

    chore: Add cursor pointer to search input

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -18,9 +18,9 @@ export default function Home() {
 
   return (
     <div className="p-4 text-zinc-100 mx-auto md:w-[40rem]">
-      <header className="flex justify-between items-center mb-6">
+      <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-bold">Encontre o seu barbeiro favorito</h1>
-      </header>
+      </div>
 
       <div className="relative mb-6">
         <Input
@@ -30,7 +30,7 @@ export default function Home() {
         />
         <Search
           onClick={handleSearchBarber}
-          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-teal-200"
+          className="absolute cursor-pointer left-3 top-1/2 transform -translate-y-1/2 text-teal-200"
         />
       </div>
 
@@ -43,8 +43,10 @@ export default function Home() {
         {!isFetching &&
           barberShop &&
           barberShop.data?.map((barber) => (
-            <div key={barber.id} className="bg-slate-100 rounded-lg p-4 flex gap-4 items-center text-gray-800">
-
+            <div
+              key={barber.id}
+              className="bg-slate-100 rounded-lg p-4 flex gap-4 items-center text-gray-800"
+            >
               <Avatar className="size-16">
                 <AvatarImage src={barber.photoUrl} />
                 <AvatarFallback>
@@ -58,10 +60,11 @@ export default function Home() {
                   {[...Array(5)].map((_, i) => (
                     <Star
                       key={`${barber.id}${i}`}
-                      className={`w-4 h-4 ${i < Math.floor(barber.rating)
+                      className={`w-4 h-4 ${
+                        i < Math.floor(barber.rating)
                           ? 'text-yellow-400 fill-yellow-400'
                           : 'text-gray-300'
-                        }`}
+                      }`}
                     />
                   ))}
                   <span className="ml-1 text-sm text-gray-600">
@@ -71,9 +74,9 @@ export default function Home() {
               </div>
               <Link
                 to={`/details-barber-shop/${barber.id}`}
-                  className='border border-slate-400 px-4 py-1 rounded-md bg-white hover:bg-primary text-black hover:text-white'
+                className="border border-slate-400 px-4 py-1 rounded-md bg-white hover:bg-primary text-black hover:text-white"
               >
-                  Ver Perfil
+                Ver Perfil
               </Link>
             </div>
           ))}
